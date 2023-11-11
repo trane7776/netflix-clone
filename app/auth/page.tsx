0e346480b7feb60b3106e17841ecb47939c98d84
trane7776 <izbasarov.t@edu.rea.ru>
2023-11-12 02:08:29 +0300

    updated

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-
+import Input from "@/components/input";
 const Auth = () => {
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -12,6 +13,20 @@ const Auth = () => {
             alt="Picture of the author"
           />
         </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <div className="flex flex-col gap-4">
+              <Input
+                label="Email"
+                onChange={() => {}}
+                id="email"
+                type="email"
+                value=""
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
