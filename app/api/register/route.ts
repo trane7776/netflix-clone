907317ba0f447b0252c2846be0cb3e1e609f4d41
trane7776 <izbasarov.t@edu.rea.ru>
2023-11-26 19:26:32 +0300

    register api router created

---
+++ app/api/register/route.ts
@@ -0,0 +1,38 @@
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+  try {
+    const { email, name, password } = await req.json();
+    const existingUser = await prismadb.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      return new Response("Email taken", {
+        status: 422,
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: "",
+        emailVerified: new Date(),
+      },
+    });
+    return new Response(`${user}`, {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify(error), {
+      status: 400,
+    });
+  }
+}
