bcd654a57f716be607bf6ff6fbf68e0722f926d6
trane7776 <izbasarov.t@edu.rea.ru>
2023-12-02 23:57:31 +0300

    updated

--- app/(routes)/profiles/page.tsx
+++ app/(routes)/profiles/page.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { useSession, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-
-export default function Home() {
+const Profiles = () => {
   const { data: user, status } = useSession();
   const router = useRouter();
   useEffect(() => {
@@ -13,14 +12,12 @@ export default function Home() {
   }, [status, router]);
   if (status === "authenticated") {
     return (
-      <>
-        <h1 className="text-3xl text-green-500 font-bold">Netflix Clone</h1>
-        <p className="text-white">Logged in as: {user?.user?.email}</p>
-        <button onClick={() => signOut()} className="h-10 w-full bg-white">
-          Logout
-        </button>
-      </>
+      <div>
+        <p className="text-white text-2xl">Profiles</p>
+      </div>
     );
   }
   return null;
-}
+};
+
+export default Profiles;
