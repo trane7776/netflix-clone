1b2293fe2fb27b059030041c1f96070ea848821b
trane7776 <izbasarov.t@edu.rea.ru>
2023-12-03 01:56:14 +0300

    completed auth routing and navigation bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useSession, signOut } from "next-auth/react";
+
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-
+import Navbar from "@/components/Navbar";
 export default function Home() {
   const { data: user, status } = useSession();
   const router = useRouter();
@@ -14,11 +15,7 @@ export default function Home() {
   if (status === "authenticated") {
     return (
       <>
-        <h1 className="text-3xl text-green-500 font-bold">Netflix Clone</h1>
-        <p className="text-white">Logged in as: {user?.user?.email}</p>
-        <button onClick={() => signOut()} className="h-10 w-full bg-white">
-          Logout
-        </button>
+        <Navbar />
       </>
     );
   }
